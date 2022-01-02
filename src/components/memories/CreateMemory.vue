96f13724a21905e5bcb67fcabbe4c5ede729e16e
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-01-02 15:53:26 +0000

    create memory details

---
+++ src/components/memories/CreateMemory.vue
@@ -0,0 +1,28 @@
+<template>
+  <form class="ion-padding">
+    <ion-list>
+      <ion-item>
+        <ion-label position="floating">Title</ion-label>
+        <ion-input type="text" required />
+      </ion-item>
+      <ion-item>
+        <ion-label position="floating">Image URL</ion-label>
+        <ion-input type="url" required />
+      </ion-item>
+      <ion-item>
+        <ion-label position="floating">Description</ion-label>
+        <ion-textarea rows="4"></ion-textarea>
+      </ion-item>
+    </ion-list>
+    <ion-button expand="full"> Save </ion-button>
+  </form>
+</template>
+
+<script>
+import { IonList, IonItem, IonLabel, IonInput, IonTextarea, IonButton } from "@ionic/vue";
+
+export default {
+  setup() {},
+  components: { IonList, IonItem, IonLabel, IonInput, IonTextarea, IonButton },
+};
+</script>
--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,12 +1,14 @@
 <template>
-  <base-layout
-    page-title="Add a Memory"
-    page-default-back-link="/memories"
-  ></base-layout>
+  <base-layout page-title="Add a Memory" page-default-back-link="/memories">
+  <create-memory-form />
+  </base-layout>
 </template>
 
 <script>
+import CreateMemoryForm from "../components/memories/CreateMemory.vue";
+
 export default {
   setup() {},
+  components: { CreateMemoryForm },
 };
 </script>
