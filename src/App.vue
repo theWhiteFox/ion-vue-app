3eea5c1223e4bf284ed23bfec48364460cf3bfcf
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-01-16 10:52:31 +0000

    header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <ion-app>
+    <ion-header />
     <ion-router-outlet />
   </ion-app>
 </template>
 
 <script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue'
+import IonHeader from './components/IonHeader.vue'
 
 export default {
   name: 'App',
   components: {
     IonApp,
-    IonRouterOutlet
+    IonRouterOutlet,
+    IonHeader
   }
 };
 </script>
