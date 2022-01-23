88411f6a79cc5265364435fe07ee7ab1fd64321d
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-01-23 15:29:35 +0000

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -37,3 +37,23 @@ const router = createRouter({
 });
 
 export default router;
+
+
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import HomePage from '@/views/Home.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
